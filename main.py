d30b468b58748af1760cebef71bf918968ea0244
杨乐达 <yangleda@outlook.com>
2019-02-17 17:01:09 +0800

    Add log text list

--- main.py
+++ main.py
@@ -31,12 +31,14 @@ class HelloFrame(wx.Frame):
         hbox_0.Add(self.start_button, proportion=1, flag=wx.EXPAND|wx.ALL, border=10)
 
         hbox_1 = wx.BoxSizer(wx.HORIZONTAL)
-        self.result_st = wx.StaticText(pnl, label="未启动")
+        self.log_tc = wx.TextCtrl(pnl, style=wx.TE_MULTILINE)
+        self.log_tc.AppendText("未启动\n")
+        #self.result_st = wx.StaticText(pnl, label="未启动")
         #font = self.st.GetFont()
         #font.PointSize += 10
         #font = font.Bold()
         #self.st.SetFont(font)
-        hbox_1.Add(self.result_st, proportion=1, flag=wx.EXPAND|wx.ALL, border=10)
+        hbox_1.Add(self.log_tc, proportion=1, flag=wx.EXPAND|wx.ALL, border=10)
 
         empty_screenshot_bitmap = wx.Image("ui/empty_screenshot.jpg", wx.BITMAP_TYPE_ANY).Scale(642, 377).ConvertToBitmap()
         self.screenshot_sb = wx.StaticBitmap(pnl, -1, empty_screenshot_bitmap)
@@ -50,9 +52,11 @@ class HelloFrame(wx.Frame):
         # 获得模拟器窗口句柄
         hwnd = win32gui.FindWindow(0, "夜神模拟器")
         if hwnd != 0:
-            self.result_st.SetLabel("窗口句柄： " + str(hwnd))
+            self.statusbar.SetStatusText("窗口句柄： " + str(hwnd))
+            self.log_tc.AppendText("窗口句柄： " + str(hwnd) + "\n")
         else:
-            self.result_st.SetLabel("未找到模拟器窗口")
+            self.statusbar.SetStatusText("未找到模拟器窗口")
+            self.log_tc.AppendText("未找到模拟器窗口\n")
 
         # 模拟器截图
         window_capture.window_capture(hwnd, "window_capture.jpg")
